52aa278c29ec6b6d2cfaf8856a548d68b02241c5
Yomkool <bannon.pj@gmail.com>
2023-05-22 19:38:50 -0400

    Init command line prompt from Node docs

---
+++ index.js
@@ -0,0 +1,12 @@
+const readLine = require('readline');
+
+const readline = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  readline.question(`What's your name?`, name => {
+    console.log(`Hi ${name}!`);
+    readline.close();
+  });
+  
